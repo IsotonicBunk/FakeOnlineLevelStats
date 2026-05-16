87cc2466fd1b366c18b91bf3e9492b78e519da53
maskagd <acebig880@gmail.com>
2026-05-17 00:06:13 +0200

    mod fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,76 @@
 // i hate c++
 #include <Geode/Geode.hpp>
 #include <Geode/ui/GeodeUI.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
 
 using namespace geode::prelude;
 
-#include <Geode/modify/LevelInfoLayer.hpp>
+// formats numbers (1234567 to 1,234,567)
+std::string getStatText(int num) {
+    std::string res = utils::numToString(num);
+
+    int insertPos = res.length() - 3;
+    while (insertPos > 0) {
+        res.insert(insertPos, ",");
+        insertPos -= 3;
+    }
+
+    return res;
+}
+
 class $modify(MyLevelInfoLayerOrSomethingIReallyDontKnowHowToNameThisLayerOrNodeIDontKnowBruhSorryForModeratorThatVerifyingThisMod, LevelInfoLayer) {
 	bool init(GJGameLevel* level, bool challenge) {
-		if (!LevelInfoLayer::init(level, challenge)) return false;
-		auto enabled = Mod::get()->getSettingValue<bool>("enabled");
+		if (!LevelInfoLayer::init(level, challenge)) 
+			return false;
 		
 		// settings button
 		auto fakeStat = CCMenuItemExt::createSpriteExtra(
 			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
 			[this](auto) { openSettingsPopup(Mod::get()); }
 		);
+		
+		// add to menu
 		auto leftMenu = this->getChildByID("left-side-menu");
-		if (auto show_btn = Mod::get()->getSettingValue<bool>("show-btn")) {
+		if (leftMenu && Mod::get()->getSettingValue<bool>("show-btn")) {
 			leftMenu->addChild(fakeStat);
 			fakeStat->setID("fake-stats-button"_spr);
 			fakeStat->setOpacity(99);
 			leftMenu->updateLayout();
 		}
 		
-		// if enabled
+		// if enabled	
+		auto enabled = Mod::get()->getSettingValue<bool>("enabled");
 		if (!enabled) return true;
 
-		// getting some vars from settings
-		auto downloads = Mod::get()->getSettingValue<std::string>("downloads");
-		auto likes = Mod::get()->getSettingValue<std::string>("likes");
-		auto length = Mod::get()->getSettingValue<std::string>("length");
-		auto is_dislike = Mod::get()->getSettingValue<bool>("is-dislike");
-		auto fix_length_pos = Mod::get()->getSettingValue<bool>("fix-length-pos");
-		auto downloads_scale_mod = Mod::get()->getSettingValue<float>("downloads-scale-mod");
-		auto likes_scale_mod = Mod::get()->getSettingValue<float>("likes-scale-mod");
-
 		// downloads
-		m_downloadsLabel->setString(downloads.c_str());
-		m_downloadsLabel->setScale(m_downloadsLabel->getScale() * downloads_scale_mod);
+		if (Mod::get()->getSettingValue<bool>("change-downloads")) {
+			auto downloads = getStatText(Mod::get()->getSettingValue<int>("downloads"));
+			this->m_downloadsLabel->setString(downloads.c_str());
+			this->m_downloadsLabel->limitLabelWidth(50.f, 0.5f, 0.1f);
+		}
 
 		// likes
-		m_likesLabel->setString(likes.c_str());
-		m_likesLabel->setScale(m_likesLabel->getScale() * likes_scale_mod);
+		if (Mod::get()->getSettingValue<bool>("change-likes")) {
+			auto likes = getStatText(Mod::get()->getSettingValue<int>("likes"));
+			this->m_likesLabel->setString(likes.c_str());
+			this->m_likesLabel->limitLabelWidth(50.f, 0.5f, 0.1f);
+		}
 
 		// length
-		m_lengthLabel->setString(length.c_str());
-		if (fix_length_pos) m_lengthLabel->setAnchorPoint(ccp(0, 0.25));
+		if (Mod::get()->getSettingValue<bool>("change-length")) {
+			auto length = Mod::get()->getSettingValue<std::string>("length");
+			this->m_lengthLabel->setString(length.c_str());
+			this->m_lengthLabel->setAnchorPoint(ccp(0, 0.25));
+		}
 
 		//set (dis)like icon
-		auto new_likes_icon = CCSpriteFrameCache::sharedSpriteFrameCache()
-    		->spriteFrameByName(is_dislike ? "GJ_dislikesIcon_001.png" : "GJ_likesIcon_001.png");
-		m_likesIcon->setDisplayFrame(new_likes_icon);
-		
+		if (Mod::get()->getSettingValue<bool>("change-is-dislike")) {
+			auto is_dislike = Mod::get()->getSettingValue<bool>("is-dislike");
+			auto likeSprName = is_dislike ? "GJ_dislikesIcon_001.png" : "GJ_likesIcon_001.png";
+			auto likeSpr = CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName(likeSprName);
+			this->m_likesIcon->setDisplayFrame(likeSpr);
+		}
+
 		return true;
 	}
 };
